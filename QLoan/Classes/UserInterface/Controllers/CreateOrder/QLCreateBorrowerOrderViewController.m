8fad1dacba7e2e197b3e26b06e496abbe5dfba4c
Mikhail Rakhmanov <m.rakhmanov@rambler-co.ru>
2016-10-22 02:51:56 +0300

    Добавил заголовок у контроллера создания заявок

--- QLoan/Classes/UserInterface/Controllers/CreateOrder/QLCreateBorrowerOrderViewController.m
+++ QLoan/Classes/UserInterface/Controllers/CreateOrder/QLCreateBorrowerOrderViewController.m
@@ -22,6 +22,9 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	[self configureTextfieldDelegate];
+	
+	self.title = @"Получить займ";
+	
 	self.monthlyCheckbox.hidden = NO;
 	self.onceCheckbox.hidden = YES;
 	
