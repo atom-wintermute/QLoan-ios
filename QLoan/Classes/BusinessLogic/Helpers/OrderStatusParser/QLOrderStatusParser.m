a9437d507ff40d0c88691af9bce171cb756f9770
Mikhail Rakhmanov <m.rakhmanov@rambler-co.ru>
2016-10-23 03:39:53 +0300

    Прикручен статус запрос к заявке

--- QLoan/Classes/BusinessLogic/Helpers/OrderStatusParser/QLOrderStatusParser.m
+++ QLoan/Classes/BusinessLogic/Helpers/OrderStatusParser/QLOrderStatusParser.m
@@ -18,12 +18,26 @@
 			return @"requested";
 		case QLBorrowerOrderStatusStarted:
 			return @"started";
+		case QLBorrowerOrderStatusFinished:
+			return @"finished";
 		default:
 			return nil;
 	}
 }
 
 + (QLBorrowerOrderStatus)statusFromDescription:(NSString *)description {
+	if ([description isEqualToString:@"active"]) {
+		return QLBorrowerOrderStatusActive;
+	}
+	if ([description isEqualToString:@"requested"]) {
+		return QLBorrowerOrderStatusRequested;
+	}
+	if ([description isEqualToString:@"started"]) {
+		return QLBorrowerOrderStatusStarted;
+	}
+	if ([description isEqualToString:@"finished"]) {
+		return QLBorrowerOrderStatusFinished;
+	}
 	return QLBorrowerOrderStatusActive;
 }
 
