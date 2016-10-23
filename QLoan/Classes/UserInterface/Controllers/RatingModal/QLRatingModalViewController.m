4bf9ee9a00051f015a696e972b07cfcdf25bc711
Nikulin Maxim <m.nikulin@rambler-co.ru>
2016-10-23 08:59:33 +0300

    Добавлен контроллер возвращения займа.

---
+++ QLoan/Classes/UserInterface/Controllers/RatingModal/QLRatingModalViewController.m
@@ -0,0 +1,33 @@
+//
+//  QLRatingModalViewController.m
+//  QLoan
+//
+//  Created by Nikulin Maksim on 23/10/16.
+//  Copyright © 2016 Rambler&Co. All rights reserved.
+//
+
+#import "QLRatingModalViewController.h"
+
+@interface QLRatingModalViewController ()
+
+@end
+
+@implementation QLRatingModalViewController
+
+#pragma mark - Жизненный цикл
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self configureAppearance];
+}
+
+#pragma mark - Приватные методы
+
+- (void)configureAppearance {
+    [self.view layoutIfNeeded];
+    
+    [self.doneButton addGradient];
+}
+
+@end
